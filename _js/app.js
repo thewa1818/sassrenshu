d2740819b3e25ac565b3cdad9d9650bd89c5610c
thewa1818 <zawa.i.0218@gmail.com>
2023-07-28 14:24:12 +0900

    kanpeki

--- _js/app.js
+++ _js/app.js
@@ -3,3 +3,9 @@ ScrollReveal().reveal('.page_top-img',{
   viewFactor: 0.6,
   reset: true
 });
+
+$(".page_top").on("click",function(){
+  let position = 0;
+  let speed = 500;
+  $("html,body").animate({scrollTop:position},speed);
+})

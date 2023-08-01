ab9ab205988129cc781fe2b5abf52da57d647461
thewa1818 <zawa.i.0218@gmail.com>
2023-08-01 23:12:40 +0900

    5

--- _js/app.js
+++ _js/app.js
@@ -1,11 +1,20 @@
 // 読み込み時
-ScrollReveal().reveal('.page_top-img',{
-  viewFactor: 0.6,
-  reset: true
+// ScrollReveal().reveal('.page_top-img',{
+//   viewFactor: 0.6,
+//   reset: true
+// });
+
+// $(".page_top").on("click",function(){
+//   let position = 0;
+//   let speed = 500;
+//   $("html,body").animate({scrollTop:position},speed);
+// })
+
+$(function(){
+  let month = new Date().getMonth() + 1;
+  let date = new Date().getDate();
+  let html = `<span class="section__wrapper-data">${month}月${date}日更新</span>`
+
+  $(".section__wrapper-title").append(html);
 });
 
-$(".page_top").on("click",function(){
-  let position = 0;
-  let speed = 500;
-  $("html,body").animate({scrollTop:position},speed);
-})

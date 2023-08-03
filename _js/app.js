307b33adbb1527dd85555e3ce536f87177199898
thewa1818 <zawa.i.0218@gmail.com>
2023-08-04 00:05:06 +0900

    999

--- _js/app.js
+++ _js/app.js
@@ -10,58 +10,78 @@
 //   $("html,body").animate({scrollTop:position},speed);
 // })
 
-$(".main__slide-img").slick({
-  arrows:false,
-  dots: false,
-  infinite: true,
-  autoplay :true,
-  autoplaySpeed: 4000,
-});
+//スライドショー
 
-$(function(){
-  let month = new Date().getMonth() + 1;
-  let date = new Date().getDate();
-  let html = `<span class="section__wrapper-data">${month}月${date}日更新</span>`
+// $(".main__slide-img").slick({
+//   arrows: false,
+//   dots: false,
+//   infinite: true,
+//   autoplay: true,
+//   autoplaySpeed: 4000,
+// });
 
-  $(".section__wrapper-title").append(html);
-});
+// //日付
 
-$(function(){
-  let items = $(".section__wrapper-item")
+// $(function () {
+//   let month = new Date().getMonth() + 1;
+//   let date = new Date().getDate();
+//   let html = `<span class="section__wrapper-data">${month}月${date}日更新</span>`;
 
-  let htmlItems = items.map((item,index)=>{
-   return items
-  })
+//   $(".section__wrapper-title").append(html);
+// });
 
-  console.log(htmlItems)
+// //ランキングのとこ
 
-  for (let i = 0; i < 10; i++) {
-    let rankingNum= `${i+1}`
-    console.log(rankingNum);
-    $(".imgNumver").append(rankingNum);
-  }
+// $(function () {
+//   const $items = $(".section__wrapper-item");
 
+//   let htmlItems = $items.map((item, index) => items);
 
-  // $(".section__wrapper-loop").append(`${htmlItems}`);
+//   console.log(htmlItems);
 
-});
+//   for (let i = 0; i < 10; i++) {
+//     let rankingNum = `${i + 1}`;
+//     console.log(rankingNum);
+//     $(".imgNumber").append(rankingNum);
+//   }
 
+//   $(".section__wrapper-loop").text(rankingNum);
+// });
 
+// //追従
 
+// $(function () {
+//   var $body = $(".section__aside-info"),
+//     $navTypeA = $(".section__aside-info"),
+//     navTypeAOffsetTop = $navTypeA.offset().top;
 
-
-
+//   $(window).on("scroll", function () {
+//     if ($(this).scrollTop() > navTypeAOffsetTop) {
+//       $body.addClass("is-fixed");
+//     } else {
+//       $body.removeClass("is-fixed");
+//     }
+//   });
+// });
 
 $(function () {
-  var $body = $('.section__aside-info'),
-      $navTypeA = $('.section__aside-info'),
-      navTypeAOffsetTop = $navTypeA.offset().top;
-  
-  $(window).on('scroll', function () {
-    if($(this).scrollTop() > navTypeAOffsetTop) {
-      $body.addClass('is-fixed');
-    } else {
-      $body.removeClass('is-fixed');
+  $(window).mousemove(function (e) {
+    console.log(e, "eee");
+
+    $("#aa").html(e.clientX);
+    $("#bb").html(e.clientY);
+
+    var tate = e.clientX;
+    var yoko = e.clientY;
+
+    if (tate <= 330 && yoko <= 40) {
+      $("#wrap").addClass("overlay");
+      $(".modal").addClass("on");
     }
+  });
+
+  $(".modal__close").on("click", function () {
+    $(".modal").removeClass("on");
+    $("#wrap").removeClass("overlay");
   });
 });

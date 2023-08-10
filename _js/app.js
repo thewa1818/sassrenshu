afb623b556f05ad6382bd3a44455c5fecb8fc8f3
thewa1818 <zawa.i.0218@gmail.com>
2023-08-10 23:13:27 +0900

    aaaaa

--- _js/app.js
+++ _js/app.js
@@ -64,152 +64,188 @@
 //   });
 // });
 
-$(function () {
-  $(window).mousemove(function (e) {
-    // console.log(e, "eee");
+// $(function () {
+//   $(window).mousemove(function (e) {
+//     // console.log(e, "eee");
 
-    // $("#aa").html(e.clientX);
-    // $("#bb").html(e.clientY);
+//     // $("#aa").html(e.clientX);
+//     // $("#bb").html(e.clientY);
 
-    var tate = e.clientX;
-    var yoko = e.clientY;
+//     var tate = e.clientX;
+//     var yoko = e.clientY;
 
-    if (tate <= 330 && yoko <= 40) {
-      $("#wrap").addClass("overlay");
-      $(".modal").addClass("on");
-    }
-  });
+//     if (tate <= 330 && yoko <= 40) {
+//       $("#wrap").addClass("overlay");
+//       $(".modal").addClass("on");
+//     }
+//   });
 
-  //会員登録
-  $(window).bind("scroll", function () {
-    scrollHeight = $(document).height();
-    scrollPosition = $(window).height() + $(window).scrollTop();
-    if ((scrollHeight - scrollPosition) / scrollHeight <= 0.0) {
-      //スクロールの位置が下部5%の範囲に来た場合
-      $("#wrap").addClass("overlay");
-      $(".modal__footer").addClass("on");
-    }
-  });
+//   //会員登録
+//   $(window).bind("scroll", function () {
+//     scrollHeight = $(document).height();
+//     scrollPosition = $(window).height() + $(window).scrollTop();
+//     if ((scrollHeight - scrollPosition) / scrollHeight <= 0.0) {
+//       //スクロールの位置が下部5%の範囲に来た場合
+//       $("#wrap").addClass("overlay");
+//       $(".modal__footer").addClass("on");
+//     }
+//   });
 
-  //三回押したら
-  var num = 1;
+//   //三回押したら
+//   var num = 1;
 
-  $(".modal__close").on("click", function () {
-    $(".modal").removeClass("on");
-    $("#wrap").removeClass("overlay");
+//   $(".modal__close").on("click", function () {
+//     $(".modal").removeClass("on");
+//     $("#wrap").removeClass("overlay");
 
-    num += 1;
+//     num += 1;
 
-    if (num === 4) {
-      $(".sale").addClass("on");
-      $("#wrap").addClass("overlay");
-      $(".sale__timer").countdowntimer({
-        minutes: 30,
-        size: "lg",
-      });
-    }
-  });
+//     if (num === 4) {
+//       $(".sale").addClass("on");
+//       $("#wrap").addClass("overlay");
+//       $(".sale__timer").countdowntimer({
+//         minutes: 30,
+//         size: "lg",
+//       });
+//     }
+//   });
 
-  $(".sale__close").on("click", function () {
-    $(".sale").removeClass("on");
-    $("#wrap").removeClass("overlay");
-  });
+//   $(".sale__close").on("click", function () {
+//     $(".sale").removeClass("on");
+//     $("#wrap").removeClass("overlay");
+//   });
 
-  $(".modal__footer__close").on("click", function () {
-    $(".modal__footer").removeClass("on");
-    $("#wrap").removeClass("overlay");
-  });
+//   $(".modal__footer__close").on("click", function () {
+//     $(".modal__footer").removeClass("on");
+//     $("#wrap").removeClass("overlay");
+//   });
+// });
+
+// $(".modal__footer__close").on("click", function () {
+//   $(".modal__footer__link").removeClass("on");
+//   $("#wrap").removeClass("overlay");
+// });
+
+// //利用規約
+// $(".modal__footer__link").on("click", function () {
+//   $(".toroku").addClass("on");
+//   $(".modal__footer").removeClass("on");
+// });
+
+// $(function () {
+//   //スクロールを含めた高さ
+//   var scroll_y = $("textarea").get(0).scrollHeight;
+
+//   //表示されている高さ
+//   var scroll_ny = $("textarea").get(0).offsetHeight;
+
+//   $("textarea").scroll(function () {
+//     //スクロール量
+//     var scroll_p = $("textarea").scrollTop();
+//     var scroll = scroll_p + scroll_ny;
+
+//     if (scroll_y < scroll) {
+//       $(".btn").fadeIn();
+//     }
+//   });
+// });
+// $(".btn").on("click", function () {
+//   $(".toroku").removeClass("on");
+//   $("#wrap").removeClass("overlay");
+// });
+
+// //追従
+
+// $(window).on("load", function () {
+//   var adjust = 0; //スクロール時のトップ位置調整用（問題なければ0）
+//   var sidebar = $(".all__wrap-main-aside"); //サイドバーを指定
+//   var wrap = $(".all__wrap-main"); //ラッパーを指定
+
+//   var adjustTop = 0;
+//   var sidebarTop = parseInt(sidebar.css("top"));
+//   var sidebarMax = wrap.height() + adjust - sidebar.height();
+
+//   $(window).on("scroll", function () {
+//     var h = sidebarTop + $(window).scrollTop();
+
+//     if (h < sidebarMax) {
+//       if ($(window).scrollTop() < adjust) {
+//         adjustTop = 0;
+//       } else {
+//         adjustTop = adjust;
+//       }
+//       var offset = sidebarTop - adjustTop + $(window).scrollTop() + "px";
+//       sidebar.animate({ top: offset }, { duration: 500, queue: false });
+//     }
+//   });
+// });
+
+// //３回目で広告をだしたい
+
+// var moveCounter = 1;
+
+// $(function () {
+//   $(window).mousemove(function (e) {
+//     // console.log(e, "aside");
+//     console.log(moveCounter, "ああああ");
+
+//     var asideX = e.clientX;
+//     var asideY = e.clientY;
+//     var move = asideX >= 1300 && asideY >= 600;
+
+//     console.log(asideX);
+//     // console.log(asideY);
+
+//     if (asideX >= 1300 && asideY >= 600) {
+//       console.log("っっっっk");
+//       moveCounter++;
+//       console.log(moveCounter, "いいいい");
+//     }
+
+//     // console.log(moveCounter);
+
+//     if (moveCounter === 3) {
+//       $(".aside").addClass("on");
+//     }
+//   });
+// });
+
+// $(".aside__close").on("click", function () {
+//   $(".aside").removeClass("on");
+// });
+
+//配列操作カスタムパラメータ
+
+var v = location.search;
+console.log(v, "sss");
+
+var vv = v.split("&");
+console.log(vv, "分割");
+
+var tt = vv.filter(function (item) {
+  return item === "utm_area=tokyo";
 });
 
-$(".modal__footer__close").on("click", function () {
-  $(".modal__footer__link").removeClass("on");
-  $("#wrap").removeClass("overlay");
+// console.log(tt, "フィルター");
+
+// if (tt.length !== 0) {
+//   alert(1);
+// }
+
+var test = location.search;
+console.log(test);
+
+var arr1 = test.split("&");
+console.log(arr1);
+
+var arr2 = arr1.filter(function (item) {
+  return item === "utm_num=2";
 });
 
-//利用規約
-$(".modal__footer__link").on("click", function () {
-  $(".toroku").addClass("on");
-  $(".modal__footer").removeClass("on");
-});
+console.log(arr2);
 
-$(function () {
-  //スクロールを含めた高さ
-  var scroll_y = $("textarea").get(0).scrollHeight;
-
-  //表示されている高さ
-  var scroll_ny = $("textarea").get(0).offsetHeight;
-
-  $("textarea").scroll(function () {
-    //スクロール量
-    var scroll_p = $("textarea").scrollTop();
-    var scroll = scroll_p + scroll_ny;
-
-    if (scroll_y < scroll) {
-      $(".btn").fadeIn();
-    }
-  });
-});
-$(".btn").on("click", function () {
-  $(".toroku").removeClass("on");
-  $("#wrap").removeClass("overlay");
-});
-
-//追従
-
-$(window).on("load", function () {
-  var adjust = 0; //スクロール時のトップ位置調整用（問題なければ0）
-  var sidebar = $(".all__wrap-main-aside"); //サイドバーを指定
-  var wrap = $(".all__wrap-main"); //ラッパーを指定
-
-  var adjustTop = 0;
-  var sidebarTop = parseInt(sidebar.css("top"));
-  var sidebarMax = wrap.height() + adjust - sidebar.height();
-
-  $(window).on("scroll", function () {
-    var h = sidebarTop + $(window).scrollTop();
-
-    if (h < sidebarMax) {
-      if ($(window).scrollTop() < adjust) {
-        adjustTop = 0;
-      } else {
-        adjustTop = adjust;
-      }
-      var offset = sidebarTop - adjustTop + $(window).scrollTop() + "px";
-      sidebar.animate({ top: offset }, { duration: 500, queue: false });
-    }
-  });
-});
-
-//３回目で広告をだしたい
-
-var moveCounter = 1;
-
-$(function () {
-  $(window).mousemove(function (e) {
-    // console.log(e, "aside");
-    console.log(moveCounter, "ああああ");
-
-    var asideX = e.clientX;
-    var asideY = e.clientY;
-    var move = asideX >= 1300 && asideY >= 600;
-
-    console.log(asideX);
-    // console.log(asideY);
-
-    if (asideX >= 1300 && asideY >= 600) {
-      console.log("っっっっk");
-      moveCounter++;
-      console.log(moveCounter, "いいいい");
-    }
-
-    // console.log(moveCounter);
-
-    if (moveCounter === 3) {
-      $(".aside").addClass("on");
-    }
-  });
-});
-
-$(".aside__close").on("click", function () {
-  $(".aside").removeClass("on");
-});
+if (arr2 === arr2) {
+  alert("二番目");
+} else if (tt === tt) {
+  alert("東京");
+}
